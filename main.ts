407eae827260588749370a7ae35b613adcf99644
xiaohai-huang <jeromehuang18@gmail.com>
2025-06-04 18:56:24 +0800

    feat: add fk, ik to main

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import five from "johnny-five";
 import Firmata from "firmata";
 import Joint from "./lib/Joint.ts";
 import { type FirmataType } from "./lib/Firmata.ts";
-import { createKinematics } from "./lib/kinematics.ts";
+import { createKinematics, Kinematics } from "./lib/kinematics.ts";
 
 export const io = new Firmata("COM3") as unknown as FirmataType;
 
@@ -16,8 +16,34 @@ board.on("ready", function () {
   const [J1, J2, J3, J4, J5, J6] = Joint.createAllJoints();
   const joints = [J1, J2, J3, J4, J5, J6];
   const kinematics = createKinematics();
-  const tf = kinematics.forwardKinematics(joints.map((joint) => joint.Degrees));
-  console.log(tf);
+  const fk = () =>
+    Kinematics.extractHomogeneousMatrix(
+      kinematics.forwardKinematics(joints.map((joint) => joint.Degrees))
+    );
 
-  board.repl.inject({ io, J1, J2, J3, J4, J5, J6, joints, kinematics });
+  type InverseKinematicsArgs = Parameters<typeof kinematics.inverseKinematics>;
+
+  const ik = (...args: InverseKinematicsArgs) => {
+    const angles = kinematics.inverseKinematics(...args);
+    console.log(angles);
+    joints.forEach((joint, index) => {
+      joint.rotateTo(angles[index]);
+    });
+  };
+  // ik(335, 0, 480, 0, -180, 0,"F");
+  board.repl.inject({
+    io,
+    J1,
+    J2,
+    J3,
+    J4,
+    J5,
+    J6,
+    joints,
+    kinematics,
+    s: Joint.stopAll,
+    Joint,
+    fk,
+    ik,
+  });
 });
